95f89c86776f54dcc7a67ecf16587ac996c52275
vincent aimar <pinz92@gmail.com>
2026-09-13 20:40:59 +0000

    feat(explore): Elev8 ride-spot chip

--- src/components/TripExploreWrapper.tsx
+++ src/components/TripExploreWrapper.tsx
@@ -36,6 +36,7 @@ const RIDE_SPOT_PRESETS: RideSpotPreset[] = [
     { label: 'Wakevilla', destination: 'Wakevilla', activity: 'wakeboard', emoji: '🇵🇹' },
     { label: 'Wake Way', destination: 'Wake Way', activity: 'wakeboard', emoji: '🇱🇹' },
     { label: 'Anupriškės', destination: 'Anupriskes', activity: 'wakeboard', emoji: '🇱🇹' },
+    { label: 'Elev8', destination: 'Elev8', activity: 'wakeboard', emoji: '🇱🇹' },
     { label: 'Les Houches', destination: 'Les Houches', activity: 'snowboard', emoji: '🏂' },
 ];
 
